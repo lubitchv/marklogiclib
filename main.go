ba5e89515dc825d9d2bd64a65f8a4bfb5de9d3fc
Victoria Lubitch <victoria.lubitch@utoronto.ca>
2026-04-27 16:20:04 -0400

    transfer

--- main.go
+++ main.go
@@ -5,14 +5,20 @@ import (
 	"fmt"
 	"github.com/icholy/digest"
 	"net/http"
+	"io"
+	"strings"
 )
 
 func PutDocument(client *MarklogicClient, uri string, data []byte, collections []string) error {
 
-	url := client.Url + "/v1/documents?uri=" + uri
+	url := client.Url + "/v1/documents?uri=" + uri 
 	for _, coll := range collections {
-		url += "&collection=" + coll
+		co := strings.Replace(coll, "#", "%23", -1)
+		co = strings.Replace(co, "/", "%2F", -1)
+		url += "&collection=" + co
 	}
+	
+	fmt.Println(url)
 	req, err := http.NewRequest(
 		"PUT",
 		url,
@@ -41,6 +47,13 @@ func PutDocument(client *MarklogicClient, uri string, data []byte, collections [
 	defer resp.Body.Close()
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
 		return fmt.Errorf("error putting document to Marklogic: %d", resp.StatusCode)
+		bodyBytes, err := io.ReadAll(resp.Body)
+		fmt.Println(string(bodyBytes))
+		if err != nil {
+			fmt.Println("error reading response body: %v", err)
+			return fmt.Errorf("error reading response body: %v", err)
+		}
+		fmt.Println(string(bodyBytes))
 	}
 	fmt.Println("Status:", resp.StatusCode)
 	return nil
